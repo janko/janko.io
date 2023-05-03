27bb1784cb685eb471018f65f8885224050f9eff
Janko Marohnić <janko.marohnic@gmail.com>
2023-05-03 10:17:16 +0200

    Make CSS nesting work

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': {},
+    tailwindcss: {},
+    autoprefixer: {},
+    cssnano: process.env.NODE_ENV === 'production' ? {} : false,
+  }
+}
